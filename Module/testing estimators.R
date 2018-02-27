159a24c361c8b3db5b31f8b834739521578fada1
SebastianGPedersen <Sebastian.g.pedersen@gmail.com>
2018-02-28 00:47:52 +0100

    Pæn opsætning af 'testing estimators' med korrekt mu.raw, sigma.raw og mu.noise men forkert sigma.noise

--- Module/testing estimators.R
+++ Module/testing estimators.R
@@ -1,55 +1,142 @@
 setwd(Sys.getenv("masters-thesis"))
+source("Kernels/kernels.R")
+source("Estimation/estimates.R")
 
-source("estimation/estimates.R")
-
-# Time setup
-t <- 0:1000  # time
-dt <- 1/t[length(t)]
+# Time parameters
+mat <- 1
+t <- 0:10000  # time_points
+dt <- mat/t[length(t)]
 
 # Define sigma
-sig2 <- 1
-omega = 10
-ksq = 0.5 # K2
-
-# omega = ((1/2)*sqrt(sig2*dt/t[length(t)]))^2
-
-
-# first, simulate a set of random deviates
-y <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt)) + rnorm(length(t), mean = 0, sd = omega)
-y <- cumsum(y)
-
-# form data and plot
-plot(y)
-dataX<-data.frame(time = t, Y = y)
-
-# estimation of sigma
-sig<-est.sigma(data = dataX, hv = 0.02, kern = kern.leftexp, wkern = kern.parzen, t.index = 500)
-
-# dt*sigma should converge in P to omega^2
-dt*sig$sig
-ksq*omega^2
+sig <- 0.1
+sig2 <- sig^2
+omega2 <- 1
+omega <- 1
+ksq <- 0.5 # K2
+hd <- 0.01 #bandwidth in mu
+hv <- 0.05 #bandwidth in sigma
 
 
-# test of convergence of mu to N(0, ksq*omega^2)
+# Simulate a Brownian motion (X) and one with mcrostrucutre noise (Y)
+x <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt))
+eps <- rnorm(length(t), mean = 0, sd = sqrt(omega))
+x <- cumsum(x)
+y <- x + eps
+
+
+# Plot part of x and y to validate it is done correctly
+plot(x[1:100],type="l")
+plot(y[1:100],type = "l")
+#Støjen overgår tydeligt X som den burde
+
+
+
+############ Test convergence of mu without noise (eq. 12 and 13) ############
+
+N <- 5000
+mu <- numeric(N)
+
+for(i in 1:N) #Takes approx 10sec
 {
-  hd<-0.02
-  N = 1000
-  mu = numeric(N)
-  for(i in 1:N)
-  {
-    sig2 <- 1
-    
-    y <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt)) + rnorm(length(t), mean = 0, sd = omega)
-    x <- cumsum(y)
-    
-    dataX<-data.frame(time = t*dt, Y = x)
-    
-    mu[i] <- sqrt(dt)*sqrt(hd)*(est.mu(dataX, hd, kern.leftexp, t.index = 500)$mu[1])
-  }
-  # properties of mu  - (   N(0,ksq*omega^2)   )
-  mean(mu)
-  var(mu)
-  plot(mu)
-  qqnorm(mu)
+  #simulate brownian motion
+  x <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt))
+  x <- cumsum(x)
+  
+  #put in data.frame for use in est.mu function
+  dataX<-data.frame(time = t*dt, Y = x)
+  
+  mu[i] <- est.mu(dataX, hd, kern.leftexp, t.index = 500)$mu[1] #Take out a random timeindex (500)
 }
+
+mu <- sqrt(hd)*mu #This should be N(0,ksq*omega^2) distributed from eq. (13)
+
+#Normalize
+mu_normal <- mu*sqrt(1/(ksq*sig2))
+
+#test - it all looks really fine
+mean(mu_normal)
+var(mu_normal)
+plot(mu_normal)
+qqnorm(mu_normal)
+
+
+
+
+############ Test convergence of sigma without noise (convergence of eq. 14 to sigma) ############
+
+#Simulate the BM
+x <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt))
+x <- cumsum(x)
+
+#Put in dataframe
+dataX<-data.frame(time = t*dt, Y = x)
+
+#Get sigma estimates on 1000 different time_points (vigtigt at bandwidth er rigtig lille)
+sigma_1000_times <-est.sigma.raw(data = dataX, hv, kern.leftexp, t.index = seq(1000, 10000, by = 10))$sigRaw
+
+est_sig2 <- sigma_1000_times^2
+
+#Test that this is close to sig2 (0.01)
+plot(est_sig2)
+
+#It looks centralized plus minus 20% so not bad
+
+
+
+
+############ Test convergence of mu with noise (convergence of eq. 25 to Theorem 5 p. 43) ############
+
+N <- 5000
+mu <- numeric(N)
+
+for(i in 1:N) #Takes approx 30sec
+{
+  #simulate brownian motion with noise
+  x <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt))
+  x <- cumsum(x)
+  eps <- rnorm(length(t), mean = 0, sd = sqrt(omega2))
+  y <- x + eps
+  
+  #put in data.frame for use in est.mu function
+  dataY<-data.frame(time = t*dt, Y = y)
+  
+  mu[i] <- est.mu(dataY, hd, kern.leftexp, t.index = 500)$mu[1] #Take out a random timeindex (500)
+}
+
+mu_ <- sqrt(hd)*mu *sqrt(dt/2) #This should be N(0,ksq*omega^2) distributed from theorem 5
+
+#Normalize
+mu_normal <- sqrt(1/(ksq*sig2))*mu_
+
+#test - it all looks really fine
+mean(mu_normal)
+var(mu_normal)
+plot(mu_normal)
+qqnorm(mu_normal)
+
+
+
+############ Test convergence of sigma with noise (convergence of eq. 25 to Theorem 5 p. 43) ############
+
+#Simulate the BM with noise
+x <- rnorm(n = length(t) , mean = 0, sd = sqrt(sig2*dt))
+x <- cumsum(x)
+eps <- rnorm(length(t), mean = 0, sd = sqrt(omega2))
+y <- x + eps
+
+#Put in dataframe
+dataY<-data.frame(time = t*dt, Y = y)
+
+#Get sigma estimates on 1000 different time_points (vigtigt at bandwidth er rigtig lille)
+sigma_1000_times <- est.sigma(data = dataY, hv = hv, kern = kern.leftexp, wkern = kern.parzen,t.index = seq(1000,10000,by=10),lag=15)$sig
+
+est_omega <- dt*sigma_1000_times #This should converge to K_2 * E(delta_eps^2) = K_2*2*E(eps^2) = K_2*2*omega2
+est_one <- est_omega/(2*ksq*omega2) #This should converge in probability to K_2*omega^2
+
+#Test that this is close to 1
+plot(est_one)
+mean(est_one)
+
+#Right now the estimate just decreases when we increase the lag. 
+# It looks as if there is missing some constant to counteract on this.
 
--- Temp/estimates_2_Seb.R
+++ Temp/estimates_2_Seb.R
@@ -6,7 +6,7 @@ est.mu <- function(data, hd, kern, t.index=NA, t.points=NA){
   # data list should include a times column and the Y column (log returns)
   # t.index should be index - we use data$time[t]
   
-  # t.points might not work correctly....
+  # t.points <- NA
   
   # kern handling
   if(is.list(kern)) kern<-kern$kern
@@ -17,8 +17,7 @@ est.mu <- function(data, hd, kern, t.index=NA, t.points=NA){
     mode = 1
     t<-data$time[1:(length(data$time))] # if nothing specified - every point in data
     ind = 1:(length(data$time))
-  }
-  else if(is.na(t.index) & !is.na(t.points)){
+  } else if(is.na(t.index) & !is.na(t.points)){
     mode = 2
     t<-t.points
     ind = numeric(length(t))
@@ -27,8 +26,7 @@ est.mu <- function(data, hd, kern, t.index=NA, t.points=NA){
     for(i in 2:length(t)){
       ind[i] = which.max(data$time[data$time<t[i]])
     }
-  }
-  else{
+  } else{
     mode = 3
     t<-data$time[t.index]
     ind = t.index
@@ -71,6 +69,12 @@ est.mu <- function(data, hd, kern, t.index=NA, t.points=NA){
 est.sigma <- function(data, hv, kern, wkern, t.index=NA, lag="auto"){   # we could do lag = "auto"
   # data list should include a times column and the Y column (log returns)
   
+  #data <- dataX
+  #kern <- kern.leftexp
+  #wkern <- kern.parzen
+  #t.index<- 500
+  #lag <- 10
+  
   # Handle lag
   if(lag=="auto") lag = 15 #temp
   
@@ -86,7 +90,7 @@ est.sigma <- function(data, hv, kern, wkern, t.index=NA, lag="auto"){   # we cou
     start = lag+1
     t<-data$time[start:(length(data$time))] # if nothing specified - every point in data
     ind<-start:(length(data$time))
-  }else{
+  } else{
     t<-data$time[t.index]
     ind<-t.index
   }
@@ -103,23 +107,24 @@ est.sigma <- function(data, hv, kern, wkern, t.index=NA, lag="auto"){   # we cou
   gamma<-function(l, t, end){
     # end is the highest needed index
     l <- abs(l)
-    out<- sum(   kern( (data$time[(l+1):(end-1)] - t)/hv )* dy[(1+l+1):end]*       #indices are literally #1 reason for bugs
-                 kern( (data$time[1:(end-l-1)] - t)/hv )* dy[2:(end-l)]   )
+    out<- sum(   kern( (data$time[(l+1):(end-1)] - t)/hv )* dy[(1+l+1):end]#*       #indices are literally #1 reason for bugs
+                 #kern( (data$time[1:(end-l-1)] - t)/hv )* dy[2:(end-l)]  
+    )
     # l <- abs(l)
     # out<- sum(    dy[(1+l+1):end]*       #indices are literally #1 reason for bugs
     #              dy[2:(end-l)]   )  
     return(out)
   }
   
-  end = n
+  end <- n
   for (j in 1:tt) {
-   sig[j] = sum(  (kern(   (data$time[1:(end-1)] - t[j])/hv   )*dy[2:end])^2  )  # l = 0
-   
+   sig[j] <- sum(  (kern(   (data$time[1:(end-1)] - t[j])/hv   )*dy[2:end]))#^2  )  # 73
    #sig[j] = sum (  dy[2:end]^2  )
-   if (lag >=1) {
+   
+   if ( l >=1) {
      for(l in 1:lag){
        #sig[j] = sig[j] + 2*(wkern(l/n)*gamma(l,t[j], end))
-       sig[j] = sig[j] + 2*(wkern(l/(lag+1))*gamma(l,t[j], end))
+       sig[j] <- sig[j] + 2*(wkern(l/(lag+1))*gamma(l,t[j], end))
      }
    }
   }
@@ -129,64 +134,3 @@ est.sigma <- function(data, hv, kern, wkern, t.index=NA, lag="auto"){   # we cou
 }
 
 
-est.sigma.raw <- function(data, hd, kern, t.index=NA, t.points=NA){
-  # data list should include a times column and the Y column (log returns)
-  # t.index should be index - we use data$time[t]
-  
-  # t.points might not work correctly....
-  
-  # kern handling
-  if(is.list(kern)) kern<-kern$kern
-  
-  # mode-handling
-  mode = NA
-  if(is.na(t.index) & is.na(t.points)){
-    mode = 1
-    t<-data$time[1:(length(data$time))] # if nothing specified - every point in data
-    ind = 1:(length(data$time))
-  }
-  else if(is.na(t.index) & !is.na(t.points)){
-    mode = 2
-    t<-t.points
-    ind = numeric(length(t))
-    #ind[1] = sum(data$time < t[1])
-    #ind[i] = data$time[ind[i-1]+sum(data$time[ind[i-1]:n] < t[i] )] #for 2 to n this may be faster than which.max
-    for(i in 2:length(t)){
-      ind[i] = which.max(data$time[data$time<t[i]])
-    }
-  }
-  else{
-    mode = 3
-    t<-data$time[t.index]
-    ind = t.index
-  }
-  #t should now be data$time points
-  
-  #n = length(t)
-  tt = length(t)
-  n = length(data$time)
-  mu = numeric(tt)          # We can only have bandwidth to end amount of calcs
-  
-  #dy = cbind(0,t(diff(t(data$Y))))               # diff only does each column seperately / so we transpose to get row wise
-  dy = c(0,diff(data$Y))
-  # we put in a column of zeros to fit our sizes - dy[i,1] should NEVER be used!
-  
-  # Optimization removed
-  if(mode == 1){
-    for(j in 1:tt){
-      mu[j] = sqrt((1/hd)*sum(kern((data$time[1:(n-1)] - t[j])/hd)*(dy[2:n])^2))   
-    }
-  }
-  else if(mode == 3){
-    for(j in 1:tt){
-      #mu[j] = (1/hd)*sum(kern((data$time[1:(ind[j]-1)] - t[j])/hd)*dy[ 2:ind[j] ]) # mu[1] = 0...?
-      mu[j] = sqrt((1/hd)*sum(kern((data$time[1:(n-1)] - t[j])/hd)*(dy[2:n])^2))
-    }
-  }
-  else{ # time points not implemented
-    mu[j] = NA
-  }
-  
-  # return list
-  return(list(time = t, sigRaw = mu))
-}
