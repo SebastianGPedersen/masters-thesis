a4b50be9ea69e628bf0bb425473073385b317e85
SebastianGPedersen <Sebastian.g.pedersen@gmail.com>
2018-05-23 12:53:53 +0200

    Added MLE rho estimation

--- Estimation/rho.R
+++ Estimation/rho.R
@@ -1,5 +1,37 @@
+#The first function is MLE and the one we should use
+#The second is Kim's OLS estimate
 
-est.rho <- function(T_vector,plot = F, arMle=F) {
+est.rho.MLE <-function(T_vector, error = 0.0001){ #With newest observation at index 1
+  
+  #T_vector <- c(1,2,3,2,4,5,1,2)
+  X_t <- T_vector[-length(T_vector)] 
+  X_t_1 <- T_vector[-1]
+  
+  N <- length(X_t)
+  
+  #Polynomium
+  third_order_pol <- function(p) {
+    value <- (1-p^2)*N*p - (1-p^2)*sum(X_t_1 * (X_t - p*X_t_1)) - p * sum((X_t - p*X_t_1)^2)
+    return(value)
+  }
+  
+  #Bisection (with p = -1 the function is positive, with p = 1 it is negative)
+  
+  lower <- -1
+  upper <- 1
+  
+  while (upper-lower > 2*error) {
+    mid <- (upper+lower)/2
+    temp_val <- third_order_pol(mid)
+    if (temp_val > 0) {lower <- mid} else {upper <- mid}
+  }
+
+  rho_est <- (upper+lower)/2
+  
+  return(rho_est)
+}
+
+est.rho.kim <- function(T_vector,plot = F, arMle=F) {
   #Input: T_vector (as vector or list)
   #Output: rho and m (in a list)
   
@@ -7,7 +39,7 @@ est.rho <- function(T_vector,plot = F, arMle=F) {
   #Check input type
   if (typeof(T_vector) == "list") {
     T_vector = unlist(T_vector)
-  # } else if (typeof(T_vector) != "vector") {
+    # } else if (typeof(T_vector) != "vector") {
   } else if (!is.vector(T_vector)) {
     stop("The input of T's has to be either in list or vector")
   }
@@ -29,13 +61,5 @@ est.rho <- function(T_vector,plot = F, arMle=F) {
   }
   
   return(list(rho = rho,m = m))
-    
-}
-
-est.rhoLoglik<-function(rho, x){
-  N<-length(x)
   
-  term1 <- ((N-1)/2)*log(1-rho^2) #Note 0 index convention
-  term2 <- (1/(2*(1-rho^2))) * sum((x[2:N]-rho*x[1:(N-1)])^2)
-  return(-(term1+term2))
 }
