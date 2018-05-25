b71e92ed9d2a3f60b01059af18bb12a683c93ebc
= <frederik.skjott@gmail.com>
2018-05-25 10:25:35 +0200

    reversals

---
+++ Temp/studyfunction testing.R
@@ -0,0 +1,7 @@
+setwd(Sys.getenv("masters-thesis"))
+source("simulation/heston.R")
+source("simulation/bursts.R")
+source("simulation/jumps.R")
+source("estimation/estimates.R")
+source("kernels/kernels.R")
+
